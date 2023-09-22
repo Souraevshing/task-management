aad6127e4e30afed47de77e515e496b1aca37b06
Sourav Kumar <isauravshing@gmail.com>
2023-09-22 15:31:34 +0530

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { Task } from './entity/task.entity';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 // passing TypeOrmModule.forFeature and passing entity name inside array, to use this entity to use repository methods
 // since orm metadata are already set at root level, therefore we will be able to use Task entity to implement CRUD operations
